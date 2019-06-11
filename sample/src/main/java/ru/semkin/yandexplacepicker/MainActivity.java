885fed8c74cef68c5efded8e66c6366deb81f664
Ivan Semkin <ivan@semkin.ru>
2019-06-12 00:08:57 +0300

    Add permission request to sample

--- sample/src/main/java/ru/semkin/yandexplacepicker/MainActivity.java
+++ sample/src/main/java/ru/semkin/yandexplacepicker/MainActivity.java
@@ -1,16 +1,20 @@
 package ru.semkin.yandexplacepicker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_PLACE_PICKER = 1001;
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 888;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showPlacePicker() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_FINE_LOCATION);
+            return;
+        }
+
         YandexPlacePicker.IntentBuilder builder = new YandexPlacePicker.IntentBuilder();
         builder.setYandexMapsKey("YOUR_MAPKIT_API_KEY"); // TODO Replace with your MapKit API key
 
@@ -32,9 +43,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if ((requestCode == REQUEST_PLACE_PICKER)) {
-            PlaceParcelable place = YandexPlacePicker.getPlace(data);
-            if (place != null) {
+            if(data != null) {
+                PlaceParcelable place = YandexPlacePicker.getPlace(data);
                 Toast.makeText(this, "You selected: " + place.getName(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showPlacePicker();
+                }
             }
         }
     }
